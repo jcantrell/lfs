b6336e01a8fc6916b6a5c8016e2f5f74782a1878
Jordan Cantrell <cantrel2@pdx.edu>
2019-11-01 21:25:38 -0700

    fixing error with ch6_9

--- Readme.md
+++ Readme.md
@@ -9,13 +9,17 @@ Step 2: Prepare lfs user environment
 Step 3: Build temporary tools
 `$ time bash lfs/src/lfs3.sh`
 Step 4: Change owner of tools to root, to avoid UID conflicts
-`$ chown -R root:root $LFS/tools`
+`export LFS=/mnt/lfs` # WHERE TO PUT THIS?
+`git clone pi@10.0.0.133:/srv/git/lfs $LFS/sources/lfs`
+`# chown -R root:root $LFS/tools`
 Step 5: Prepare nodes
 `# bash lfs/src/lfs4.sh`
 Step 6: Change to chroot environment (source the script so it will see
   environment variables)
 `# . lfs/src/lfs5.sh`
-Step 7: Obtain lfs scripts within chroot environment
-`wget 10.0.0.133/lfs/9.0/src/lfs6.sh`
+#Step 7: Obtain lfs scripts within chroot environment
+#`wget 10.0.0.133/lfs/9.0/src/lfs6.sh`
 Step 8: Run new bash
 `$ bash lfs/src/lfs6.sh`
+Step 9: Begin building sources
+`$ bash lfs/src/lfs7.sh`
--- src/lfs7.sh
+++ src/lfs7.sh
@@ -1,4 +1,8 @@
 set -xe
+set -o functrace
+
+LP="/sources/lfs/logs"
+mkdir -p $LP
 
 # This file follows lfs chapter 6, section 6 through 34 when
 # the environment is changed to use the newly-compiled bash shell
@@ -22,73 +26,64 @@ ch6_8() {
 }
 
 ch6_9() {
-  patch -Np1 -i ../glibc-2.21-fhs-1.patch
-
-  sed -i '/asm.socket.h/a# include <linux/sockios.h>' \
-    sysdeps/unix/sysv/linux/bits/socket.h
-
-  case $(uname -m) in
-      i?86)   ln -sfv ld-linux.so.2 /lib/ld-lsb.so.3
-      ;;
-      x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64
-              ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
-      ;;
-  esac
-
-  mkdir -v build
-  cd build
-
-  CC="gcc -ffile-prefix-map=/tools=/usr" \
-  ../configure --prefix=/usr                          \
-               --disable-werror                       \
-               --enable-kernel=3.2                    \
-               --enable-stack-protector=strong        \
-               --with-headers=/usr/include            \
-               libc_cv_slibdir=/lib
-  make
-
-  case $(uname -m) in
-    i?86)   ln -sfnv $PWD/elf/ld-linux.so.2        /lib ;;
-    x86_64) ln -sfnv $PWD/elf/ld-linux-x86-64.so.2 /lib ;;
-  esac
-
-  make check
-  touch /etc/ld.so.conf
-  sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
-  make install
-
-  cp -v ../nscd/nscd.conf /etc/nscd.conf
-  mkdir -pv /var/cache/nscd
-
-  mkdir -pv /usr/lib/locale
-  localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true
-  localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
-  localedef -i de_DE -f ISO-8859-1 de_DE
-  localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
-  localedef -i de_DE -f UTF-8 de_DE.UTF-8
-  localedef -i el_GR -f ISO-8859-7 el_GR
-  localedef -i en_GB -f UTF-8 en_GB.UTF-8
-  localedef -i en_HK -f ISO-8859-1 en_HK
-  localedef -i en_PH -f ISO-8859-1 en_PH
-  localedef -i en_US -f ISO-8859-1 en_US
-  localedef -i en_US -f UTF-8 en_US.UTF-8
-  localedef -i es_MX -f ISO-8859-1 es_MX
-  localedef -i fa_IR -f UTF-8 fa_IR
-  localedef -i fr_FR -f ISO-8859-1 fr_FR
-  localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
-  localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
-  localedef -i it_IT -f ISO-8859-1 it_IT
-  localedef -i it_IT -f UTF-8 it_IT.UTF-8
-  localedef -i ja_JP -f EUC-JP ja_JP
-  localedef -i ja_JP -f SHIFT_JIS ja_JP.SIJS 2> /dev/null || true
-  localedef -i ja_JP -f UTF-8 ja_JP.UTF-8
-  localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
-  localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
-  localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
-  localedef -i zh_CN -f GB18030 zh_CN.GB18030
-  localedef -i zh_HK -f BIG5-HKSCS zh_HK.BIG5-HKSCS
-
-  make localedata/install-locales
+patch -Np1 -i ../glibc-2.30-fhs-1.patch
+sed -i '/asm.socket.h/a# include <linux/sockios.h>' \
+   sysdeps/unix/sysv/linux/bits/socket.h
+case $(uname -m) in
+    i?86)   ln -sfv ld-linux.so.2 /lib/ld-lsb.so.3
+    ;;
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
+    ;;
+esac
+mkdir -v build
+cd       build
+CC="gcc -ffile-prefix-map=/tools=/usr" \
+../configure --prefix=/usr                          \
+             --disable-werror                       \
+             --enable-kernel=3.2                    \
+             --enable-stack-protector=strong        \
+             --with-headers=/usr/include            \
+             libc_cv_slibdir=/lib
+make
+case $(uname -m) in
+  i?86)   ln -sfnv $PWD/elf/ld-linux.so.2        /lib ;;
+  x86_64) ln -sfnv $PWD/elf/ld-linux-x86-64.so.2 /lib ;;
+esac
+make check
+touch /etc/ld.so.conf
+sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
+make install
+cp -v ../nscd/nscd.conf /etc/nscd.conf
+mkdir -pv /var/cache/nscd
+mkdir -pv /usr/lib/locale
+localedef -i POSIX -f UTF-8 C.UTF-8 2> /dev/null || true
+localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
+localedef -i de_DE -f ISO-8859-1 de_DE
+localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
+localedef -i de_DE -f UTF-8 de_DE.UTF-8
+localedef -i el_GR -f ISO-8859-7 el_GR
+localedef -i en_GB -f UTF-8 en_GB.UTF-8
+localedef -i en_HK -f ISO-8859-1 en_HK
+localedef -i en_PH -f ISO-8859-1 en_PH
+localedef -i en_US -f ISO-8859-1 en_US
+localedef -i en_US -f UTF-8 en_US.UTF-8
+localedef -i es_MX -f ISO-8859-1 es_MX
+localedef -i fa_IR -f UTF-8 fa_IR
+localedef -i fr_FR -f ISO-8859-1 fr_FR
+localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
+localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
+localedef -i it_IT -f ISO-8859-1 it_IT
+localedef -i it_IT -f UTF-8 it_IT.UTF-8
+localedef -i ja_JP -f EUC-JP ja_JP
+localedef -i ja_JP -f SHIFT_JIS ja_JP.SIJS 2> /dev/null || true
+localedef -i ja_JP -f UTF-8 ja_JP.UTF-8
+localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
+localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
+localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
+localedef -i zh_CN -f GB18030 zh_CN.GB18030
+localedef -i zh_HK -f BIG5-HKSCS zh_HK.BIG5-HKSCS
+make localedata/install-locales
 
 cat > /etc/nsswitch.conf << "EOF"
 # Begin /etc/nsswitch.conf
@@ -108,24 +103,23 @@ rpc: files
 # End /etc/nsswitch.conf
 EOF
 
-  tar -xf ../../tzdata2019b.tar.gz
-  
-  ZONEINFO=/usr/share/zoneinfo
-  mkdir -pv $ZONEINFO/{posix,right}
-  
-  for tz in etcetera southamerica northamerica europe africa antarctica  \
-            asia australasia backward pacificnew systemv; do
-      zic -L /dev/null   -d $ZONEINFO       ${tz}
-      zic -L /dev/null   -d $ZONEINFO/posix ${tz}
-      zic -L leapseconds -d $ZONEINFO/right ${tz}
-  done
-  
-  cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
-  zic -d $ZONEINFO -p America/New_York
-  unset ZONEINFO
+tar -xf ../../tzdata2019b.tar.gz
 
+ZONEINFO=/usr/share/zoneinfo
+mkdir -pv $ZONEINFO/{posix,right}
 
-  ln -sfv /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+for tz in etcetera southamerica northamerica europe africa antarctica  \
+          asia australasia backward pacificnew systemv; do
+    zic -L /dev/null   -d $ZONEINFO       ${tz}
+    zic -L /dev/null   -d $ZONEINFO/posix ${tz}
+    zic -L leapseconds -d $ZONEINFO/right ${tz}
+done
+
+cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+zic -d $ZONEINFO -p America/New_York
+unset ZONEINFO
+
+ln -sfv /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
 cat > /etc/ld.so.conf << "EOF"
 # Begin /etc/ld.so.conf
@@ -140,7 +134,6 @@ include /etc/ld.so.conf.d/*.conf
 
 EOF
 mkdir -pv /etc/ld.so.conf.d
-
 
   cd ..
 }
@@ -496,9 +489,9 @@ bs() { # package, extension, chapternum
   $NUM >$LP/$NUM.log 2>$LP/$NUM.err
   cd ..
   rm -rf $PK
-  #bi $PK $EXT ch5_$NUM >$LP/5.$NUM.log 2>$LP/5.$NUM.err
 }
 
+cd /sources
 ch6_6
 bs ch6_7 linux-5.2.8    .tar.xz
 bs ch6_8 man-pages-5.02 .tar.xz
